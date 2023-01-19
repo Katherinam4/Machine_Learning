489cfb5a99751b57b3abcaadcd3972e0a0615f07
Katherinam4 <84234484+Katherinam4@users.noreply.github.com>
2023-01-19 22:11:18 +0400

    Update Final_prep.py

--- Final_prep.py
+++ Final_prep.py
@@ -1,5 +1,4 @@
 # TASK1
-
 # import pandas as pd
 # from sklearn.model_selection import train_test_split
 # from sklearn.linear_model import LogisticRegression
@@ -14,9 +13,11 @@
 # algo = LogisticRegression(max_iter=500000, C=0.7)
 # algo.fit(X_train, y_train)
 # print(algo.score(X_test, y_test))
-
+# print(algo.score(X_train, y_train))
+# 
+# 
 # TASK2
-
+# 
 # import pandas as pd
 # from sklearn.feature_selection import f_classif, SelectKBest
 # from sklearn.model_selection import train_test_split
@@ -29,15 +30,42 @@
 # X = data.drop('Prediction', axis=1).values
 # y = data['Prediction'].values
 #
-# X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.3)
-# algo1 = SVC()
-# algo1.fit(X_train, y_train)
-# print(algo1.score(X_test, y_test))
+# X_test, X_train, y_test, y_train = train_test_split(X, y, test_size=0.3, random_state=1)
 #
 # selector = SelectKBest(score_func=f_classif, k=4)
 # X_new = selector.fit_transform(X, y)
-# X_train, X_test, y_train, y_test = train_test_split(X_new, y, random_state=1, test_size=0.3)
-# algo2 = SVC()
-# algo2.fit(X_train, y_train)
-# print(algo2.score(X_test, y_test))
+#
+# algo1 = SVC()
+# algo1.fit(X_train, y_train)
+# print(algo1.score(X_test, y_test))
+# print(algo1.score(X_train, y_train))
+
+# TASK3
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import Lasso
+# from sklearn.preprocessing import StandardScaler
+#
+#
+# data = pd.read_csv('regression.csv')
+# data = data.dropna()
+# X = data.drop('x', axis=1)
+# y = data['x']
+#
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=1)
+#
+# X_train = X_train.values.reshape(-1, 1)
+# X_test = X_test.values.reshape(-1, 1)
+#
+# scaler = StandardScaler()
+# scaler.fit_transform(X_train, y_train)
+# scaler.fit_transform(X_test, y_test)
+#
+# lasso = Lasso()
+# lasso.fit(X_train, y_train)
+#
+# print(lasso.score(X_test, y_test))
+# print(lasso.score(X_train, y_train))
+
 
